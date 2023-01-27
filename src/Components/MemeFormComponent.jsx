7caba2c4e973d6544e6a4f2baf515bb5b5cb3a16
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-01-27 16:14:29 +0530

    Done!

--- src/Components/MemeFormComponent.jsx
+++ src/Components/MemeFormComponent.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import memeData from '../MemesData'
-
 export default function MemeFormComponent() {
     const [memeImage, setMemeImage] = useState("")
     function getImageUrl() {
         const randomIndex = Math.floor(Math.random() * memeData.data.memes.length)
         const item = memeData.data.memes[randomIndex]
         setMemeImage(item.url)
-        console.log(memeImage)
     }
     return (
         <div className='main'>
