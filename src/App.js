5ab5f0f535b083cbd50b8b61124a24b7c8ad67e4
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-01-27 15:56:08 +0530

    meme image generated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
+import MemeFormComponent from './Components/MemeFormComponent';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MemeFormComponent/>
+      
     </div>
   );
 }
---
+++ src/Components/MemeFormComponent.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import memeData from '../MemesData'
+
+export default function MemeFormComponent() {
+    const [memeImage, setMemeImage] = useState("")
+    function getImageUrl() {
+        const randomIndex = Math.floor(Math.random() * memeData.data.memes.length)
+        const item = memeData.data.memes[randomIndex]
+        setMemeImage(item.url)
+        console.log(memeImage)
+    }
+    return (
+        <div className='main'>
+            <div className='form'>
+                <input type="text" placeholder='Top text'></input>
+                <input type="text" placeholder='Bottom text'></input>
+                <button onClick={getImageUrl}>Get a new meme image</button>
+                <img src={memeImage} alt="" className='meme-image' />
+            </div>
+        </div>
+    )
+}
